bd7b1495e64383e2cba161bbef07cc9676a8a048
Anshika Pal <anshikapal7174@gmail.com>
2026-06-12 14:45:04 +0530

    Add files via upload

---
+++ JAVA/Chapter2/chapter2.java
@@ -0,0 +1,35 @@
+package Chapter2;
+// public class chapter2 {
+//     public static void main(String[] args) {
+//         System.out.print("Good Morning");
+//     }
+// }
+
+
+// public class chapter2 {
+//     public static void main(String[] args) {
+//         System.out.println("*");
+//         System.out.println("* *");
+//         System.out.println("* * *");
+//         System.out.println("* * * *");
+//         System.out.println("* * * * *");
+
+
+//     }
+ 
+    
+// }
+
+public class chapter2 {
+    public static void main(String[] args) {
+    
+        System.out.println("* * * * *");
+        System.out.println("* * * *  ");
+        System.out.println("* * *  ");
+        System.out.println("* * ");
+        System.out.println("*  ");
+        
+    }
+ 
+    
+}
